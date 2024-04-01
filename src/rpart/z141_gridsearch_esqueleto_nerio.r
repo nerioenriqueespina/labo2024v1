0ed308fa65e9d072b062072b56660490134499bf
nerioenriqueespina <neespinaestrada@mail.austral.edu.ar>
2024-03-31 23:22:36 -0300

    Segunda iteración con diferentes parámetros

--- src/rpart/z141_gridsearch_esqueleto_nerio.r
+++ src/rpart/z141_gridsearch_esqueleto_nerio.r
@@ -117,23 +117,27 @@ tb_grid_search <- data.table( max_depth = integer(),
 for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
   for (vmin_split in c(1000, 800, 600, 400, 200, 100, 50, 20, 10)) {
     # notar como se agrega
+    for (vminbucket in c(2, 10, 20)){
+      for (vcp in c(-1, 0, 1)){
 
-    # vminsplit  minima cantidad de registros en un nodo para hacer el split
-    param_basicos <- list(
-      "cp" = -0.5, # complejidad minima
-      "minsplit" = vmin_split,
-      "minbucket" = 5, # minima cantidad de registros en una hoja
-      "maxdepth" = vmax_depth
-    ) # profundidad máxima del arbol
+        # vminsplit  minima cantidad de registros en un nodo para hacer el split
+        param_basicos <- list(
+          "cp" = vcp, # complejidad minima
+          "minsplit" = vmin_split,
+          "minbucket" = vminbucket, # minima cantidad de registros en una hoja
+          "maxdepth" = vmax_depth
+        ) # profundidad máxima del arbol
 
-    # Un solo llamado, con la semilla 17
-    ganancia_promedio <- ArbolesMontecarlo(PARAM$semillas, param_basicos)
+        # Un solo llamado, con la semilla 17
+        ganancia_promedio <- ArbolesMontecarlo(PARAM$semillas, param_basicos)
 
-    # agrego a la tabla
-    tb_grid_search <- rbindlist( 
-      list( tb_grid_search, 
-            list( vmax_depth, vmin_split, ganancia_promedio) ) )
+        # agrego a la tabla
+        tb_grid_search <- rbindlist( 
+          list( tb_grid_search, 
+                list( vcp, vmax_depth, vmin_split, vminbucket, ganancia_promedio) ) )
 
+      }
+    }
   }
 
   # escribo la tabla a disco en cada vuelta del loop mas externo
